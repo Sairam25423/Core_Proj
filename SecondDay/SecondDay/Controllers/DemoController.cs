7c256898151ffc8f7ac6895cc1e2e80c366f069d
Sairam Bolloju <sairamb.netdeveloper@gmail.com>
2025-06-06 12:25:10 +0530

    Initial commit

---
+++ SecondDay/SecondDay/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondDay.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is the About page from DemoController";
+            return View();
+        }
+    }
+}
