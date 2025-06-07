2b902a9e3a2904238f3ef72eea5f2d30b3ac499b
Sairam Bolloju <sairamb.netdeveloper@gmail.com>
2025-06-07 22:08:33 +0530

    Add DayThreeVal

---
+++ DayThreeVal/DayThreeVal/Controllers/DemoController.cs
@@ -0,0 +1,31 @@
+﻿using DayThreeVal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayThreeVal.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user) {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Welcome {user.Name}, Age:{user.Age}, Email:{user.Email}";
+                return View("Success");
+            }
+            return View(user);
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ DayThreeVal/DayThreeVal/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DayThreeVal.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60.")]
+        public int Age { get; set; }
+    }
+}
